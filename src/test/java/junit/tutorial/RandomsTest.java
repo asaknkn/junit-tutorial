234d9387cce55dcc56c00b83fb20ce66bdb25d8e
Kentarou Asai <asaknkn@gmail.com>
2019-10-20 15:14:55 +0900

    モックの利用

--- src/test/java/junit/tutorial/RandomsTest.java
+++ src/test/java/junit/tutorial/RandomsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -27,14 +28,30 @@ public class RandomsTest {
         options.add("A");
         options.add("B");
         Randoms sut = new Randoms();
-        //スタブ実装
+        //モックの設定
+        final AtomicBoolean isCallNextIntMethod = new AtomicBoolean(false);
+        //スタブ実装(匿名クラス使用)
         sut.generator = new RandomNumberGenerator() {
             @Override
             public int nextInt() {
+                isCallNextIntMethod.set(true);
                 return 1;
             }
         };
         assertThat(sut.choice(options), is("B"));
+        assertThat(isCallNextIntMethod.get(), is(true));
+    }
+
+    @Test
+    public void choiceでCを返す() throws Exception {
+        List<String> options = new ArrayList<String>();
+        options.add("A");
+        options.add("B");
+        options.add("C");
+        Randoms sut = new Randoms();
+        //スタブ実装(ラムダの使用)
+        sut.generator = () -> {return 2;};
+        assertThat(sut.choice(options), is("C"));
     }
 
 }
